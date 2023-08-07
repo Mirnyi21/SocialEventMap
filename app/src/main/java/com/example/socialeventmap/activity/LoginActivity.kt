c4c5eea05c813f917097c7700ce722594bd0c168
Timur Mirnyi <bobrinoeschaste@gmail.com>
2023-08-07 19:28:46 +0600

    dobavil activity

---
+++ app/src/main/java/com/example/socialeventmap/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialeventmap.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialeventmap.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/example/socialeventmap/activity/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialeventmap.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialeventmap.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_activity)
+    }
+}
